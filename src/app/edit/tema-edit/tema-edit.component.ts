d500d2818c2a0412f5fda6f6b774d2be009531a8
lucas bentes <lucasbentes16@gmail.com>
2022-08-23 14:57:14 -0300

    pequenas mudancas

--- src/app/edit/tema-edit/tema-edit.component.ts
+++ src/app/edit/tema-edit/tema-edit.component.ts
@@ -22,7 +22,7 @@ export class TemaEditComponent implements OnInit {
   ngOnInit(){
     window.scroll(0,0)
     if(environment.token == ""){
-      alert("Sua seção expirou, faça o login novamente")
+      alert("Sua sessão expirou, faça o login novamente")
       this.router.navigate(["/login"])
     }
     let id = this.route.snapshot.params["id"]
